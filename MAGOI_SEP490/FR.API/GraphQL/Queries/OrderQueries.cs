1ee7e22a092b6311875d568d257bdef83a1a58d5
LanBTH <lanbthhe160676@fpt.edu.vn>
2024-02-05 22:21:14 +0700

    [feature] Get Food List by order

--- MAGOI_SEP490/FR.API/GraphQL/Queries/OrderQueries.cs
+++ MAGOI_SEP490/FR.API/GraphQL/Queries/OrderQueries.cs
@@ -6,5 +6,7 @@ namespace FR.API.GraphQL.Queries
     public partial class Queries
     {
         public List<Order> GetOrdersByStatusID(IOrderService service,int? statusID) => service.GetOrdersByOrderStatusId(statusID);
+
+        public List<FoodOrder> GetFoodOrdersByOrderId (IFoodOrderService service, Guid orderId) => service.GetFoodOrdersByOrderId(orderId);
     }
 }
