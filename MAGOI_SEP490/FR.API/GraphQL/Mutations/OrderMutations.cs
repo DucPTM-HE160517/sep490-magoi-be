0580faa83b8e42fa9d9091bc3353112a862d5e10
DuyDN <dongocduy.duydo@gmail.com>
2024-02-21 16:25:35 +0700

    [feat]: update exponotification service

--- MAGOI_SEP490/FR.API/GraphQL/Mutations/OrderMutations.cs
+++ MAGOI_SEP490/FR.API/GraphQL/Mutations/OrderMutations.cs
@@ -11,11 +11,11 @@ namespace FR.API.GraphQL.Mutations
     public partial class Mutation
     {
         public async Task<AddOrderPayload> AddOrder(
-            [Service] ITopicEventSender eventSender, CancellationToken cancellationToken,
+            [Service] ITopicEventSender eventSender, 
             IOrderService orderService, IFoodOrderService foodOrderService,
             ITableService tableService, IFoodService foodService,
             OrderInput orderInput,
-            List<FoodOrderInput> foodListInput)
+            List<FoodOrderInput> foodListInput, CancellationToken cancellationToken)
         {
             //check order: foods amount exceed food quantity
             if (!foodService.CheckFoodOrdersQuantity(foodListInput))
@@ -66,28 +66,10 @@ namespace FR.API.GraphQL.Mutations
             //get list of waiter devices
             List<string> waiterTokens = sessionService.GetExpoTokensByRoleId("waiter");
 
-            //create notification
-            var pushTicketReq = new PushTicketRequest()
-            {
-                PushTo = waiterTokens,
-
-                //TODO: change table name and order number to appropriate value
-                PushTitle = $"Table {1} - Order {"FR1234"}",
-                PushBody = "There is a done food! Please serve the food to the customer!",
-            };
-
-            //send notification to expo server
-            var result = await expoSdkClient.PushSendAsync(pushTicketReq);
-
-            //handle error
-            if (result?.PushTicketErrors?.Count() > 0)
-            {
-                foreach (var error in result.PushTicketErrors)
-                {
-                    Console.WriteLine($"Error: {error.ErrorCode} - {error.ErrorMessage}");
-                }
-            }
-
+            await expoSdkClient.SendNotification(waiterTokens, 
+                $"Table {1} - Order {"FR1234"}", 
+                "There is a done food! Please serve the food to the customer!");
+           
             return "Notification sent!";
         }
     }
--- MAGOI_SEP490/FR.Services/Service/ExpoNotificationService.cs
+++ MAGOI_SEP490/FR.Services/Service/ExpoNotificationService.cs
@@ -65,6 +65,29 @@ namespace FR.Services.Service
             var ticketResponse = await PostAsync<PushTicketRequest, PushTicketResponse>(pushTicketRequest, PushSendPath);
             return ticketResponse;
         }
+
+        public async Task SendNotification(List<String> pushToDeviceTokens ,String title, String body)
+        {
+            //create notification
+            var pushTicketReq = new PushTicketRequest()
+            {
+                PushTo = pushToDeviceTokens,
+                PushTitle = title,
+                PushBody = body,
+            };
+
+            //send notification to expo server
+            var result = await PushSendAsync(pushTicketReq);
+
+            //handle error
+            if (result?.PushTicketErrors?.Count() > 0)
+            {
+                foreach (var error in result.PushTicketErrors)
+                {
+                    Console.WriteLine($"Error: {error.ErrorCode} - {error.ErrorMessage}");
+                }
+            }
+        }
     }
 }
 
