a676dbf3330d7ea406cf630f97b00977eb54d40d
ErnestaT002 <135521162+ErnestaT002@users.noreply.github.com>
2024-03-27 22:22:01 +0700

    [fix][Bring FoodOrder view to FoodOrder Queries]

---
+++ MAGOI_SEP490/FR.API/GraphQL/Queries/FoodOrderQueries.cs
@@ -0,0 +1,10 @@
+﻿using FR.BusinessObjects.Models;
+using FR.Services.IService;
+
+namespace FR.API.GraphQL.Queries
+{
+    public partial class Queries
+    {
+        public List<FoodOrder> GetFoodOrdersByOrderId(IFoodOrderService service, Guid orderId) => service.GetFoodOrdersByOrderId(orderId);
+    }
+}
