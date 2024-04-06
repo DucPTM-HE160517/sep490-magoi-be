b66e4aca591c0d0fb9a8e2d6a57cff04846a7a8e
DuyDN <dongocduy.duydo@gmail.com>
2024-04-06 22:11:29 +0700

    [refactor]: update sale record query

--- MAGOI_SEP490/FR.API/GraphQL/Queries/SaleReportQueries.cs
+++ MAGOI_SEP490/FR.API/GraphQL/Queries/SaleReportQueries.cs
@@ -5,7 +5,7 @@ namespace FR.API.GraphQL.Queries
 {
     public partial class Queries
     {
-        public SaleReport GetSaleReports(IFoodOrderService foodOrderService, DateTime date) 
-                                 => foodOrderService.GetSaleReport(date);
+        public SaleReport GetSaleReports(IFoodOrderService foodOrderService, DateTime startDate, DateTime endDate) 
+                                 => foodOrderService.GetSaleReport(startDate, endDate);
     }
 }
