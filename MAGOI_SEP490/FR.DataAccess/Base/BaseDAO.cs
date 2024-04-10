3d972069df7d295c7bbfa0395cc4675fd6bd9e45
DuyDN <dongocduy.duydo@gmail.com>
2024-04-11 01:47:55 +0700

    [refactor]: add base

---
+++ MAGOI_SEP490/FR.DataAccess/Base/BaseDAO.cs
@@ -0,0 +1,32 @@
+﻿using FR.BusinessObjects.DataContext;
+using FR.BusinessObjects.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FR.DataAccess.Base
+{
+    public class BaseDAO<T> : IBaseDAO<T> where T : BaseModel
+    {
+        protected readonly DBContext _context;
+        public BaseDAO(DBContext context)
+        {
+            _context = context;
+        }
+        public async Task AddAsync(T entity)
+        {
+            await _context.Set<T>().AddAsync(entity);
+        }
+        public void DeleteAsync(T entity)
+        {
+            _context.Set<T>().Remove(entity);
+        }
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await _context.Set<T>().ToListAsync();
+        }
+
+        public void UpdateAsync(T entity)
+        {
+            _context.Entry(entity).State = EntityState.Modified;
+        }
+    }
+}
---
+++ MAGOI_SEP490/FR.DataAccess/Base/IBaseDAO.cs
@@ -0,0 +1,12 @@
+﻿using FR.BusinessObjects.Models;
+
+namespace FR.DataAccess.Base
+{
+    public interface IBaseDAO<T> where T : BaseModel
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+        Task AddAsync(T entity);
+        void UpdateAsync(T entity);
+        void DeleteAsync(T entity);
+    }
+}
