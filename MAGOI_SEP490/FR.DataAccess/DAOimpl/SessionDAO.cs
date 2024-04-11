3ce249fae9fac40e68128695b793251e03e7a0d5
ErnestaT002 <135521162+ErnestaT002@users.noreply.github.com>
2024-04-11 22:49:30 +0700

    Fix/Update code of SessionDAO with UOW structure

--- MAGOI_SEP490/FR.DataAccess/DAOimpl/SessionDAO.cs
+++ MAGOI_SEP490/FR.DataAccess/DAOimpl/SessionDAO.cs
@@ -13,17 +13,34 @@ public class SessionDAO : BaseDAO<Session>, ISessionDAO
 
     public IQueryable<string> GetExpoTokensByRoleId(string roleId)
     {
-        throw new NotImplementedException();
+        return _context.Sessions.Where(s => s.Role == roleId).Select(s => s.ExpoToken);
     }
 
     public Task RegisterSession(string expoToken, string roleId)
     {
-        throw new NotImplementedException();
+        return Task.Run(() =>
+        {
+            // Check if session already exists
+            var session = _context.Sessions.FirstOrDefault(s => s.ExpoToken == expoToken);
+            if (session != null)
+            {
+                return;
+            }
+
+            // Add new session
+            _context.Sessions.Add(new Session()
+            {
+                ExpoToken = expoToken,
+                Role = roleId
+            });
+
+            _context.SaveChanges();
+        });
     }
 
     //public void RegisterSession(string expoToken, string roleId)
     //{
-    //    //Check if session already exists
+    //    Check if session already exists
     //    var session = _context.Sessions.FirstOrDefault(s => s.ExpoToken == expoToken);
     //    if (session != null)
     //    {
