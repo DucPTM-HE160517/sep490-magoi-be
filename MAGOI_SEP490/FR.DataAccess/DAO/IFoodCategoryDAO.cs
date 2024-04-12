e0d27f59d27c978098b5269b3047f628861ef561
ErnestaT002 <135521162+ErnestaT002@users.noreply.github.com>
2024-04-12 20:27:05 +0700

    fix/update IBaseDao Class of FoodCategory

--- MAGOI_SEP490/FR.DataAccess/DAO/IFoodCategoryDAO.cs
+++ MAGOI_SEP490/FR.DataAccess/DAO/IFoodCategoryDAO.cs
@@ -8,7 +8,7 @@ using FR.DataAccess.Base;
 
 namespace FR.DataAccess.DAO
 {
-    public interface IFoodCategoryDAO : IBaseDAO<Feedback>
+    public interface IFoodCategoryDAO : IBaseDAO<FoodCategory>
     {
         Task<FoodCategory> GetById(int id);
         Task<IEnumerable<FoodCategory>> GetCategoryOfCookingFoods();
